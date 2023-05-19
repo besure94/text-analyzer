67c4fd81adf47443e24359989b65d60a0e5f26cf
Brian Scherner <brianscherner@gmail.com>
2023-05-19 15:37:26 -0700

    Add handleFormSubmission function defining passage and word by their id's from HTML file. Define 'wordCount' using wordCounter function using passage variable as argument. Define occurrencesOfWord as numberOfOccurrencesInText function using 'word' and 'passage' as arguments. Get innerText from 'total-count' and 'selected-account' id's. Add eventListener for 'load' function selecting 'word-counter' form id and submitting handleFormSubmission function.

--- js/scripts.js
+++ js/scripts.js
@@ -39,3 +39,17 @@ function numberOfOccurrencesInText(word, text) {
 // const passage = "zoinks muppeteer biffaroni loopdaloop The four men split up and walked off in different directions.";
 // const offensiveTerms = "zoinks muppeteer biffaroni loopdaloop";
 // console.log("New passage: ", omitOffensiveTerms(passage, offensiveTerms));
+
+function handleFormSubmission() {
+	event.preventDefault();
+	const passage = document.getElementById("text-passage").value;
+	const word = document.getElementById("word").value;
+	const wordCount = wordCounter(passage);
+	const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+	document.getElementById("total-count").innerText = wordCount;
+	document.getElementById("selected-account").innerText = occurrencesOfWord;
+}
+
+window.addEventListener("load", function() {
+	document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+});
