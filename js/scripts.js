92982b84e215dd59a9bc7c4cfae5494c06c21197
Brian Scherner <brianscherner@gmail.com>
2023-05-19 15:05:43 -0700

    Comment out function for omitting words from passage.

--- js/scripts.js
+++ js/scripts.js
@@ -12,11 +12,6 @@ function wordCounter(inputText) {
 	return wordCount;
 }
 
-const text2 = "hello there";
-wordCounter("");
-// console.log(wordCounter("hi there 77 19"));
-// console.log("Word count: ", wordCounter(text2));
-
 function numberOfOccurrencesInText(word, text) {
 	const textArray = text.split(" ");
 	let wordCount = 0;
@@ -28,21 +23,19 @@ function numberOfOccurrencesInText(word, text) {
 	return wordCount;
 }
 
-const text = "red blue red red red green";
-const word = "red";
-console.log("Occurrence count: ", numberOfOccurrencesInText(word, text));
+// Practice function for omitting words from passage//
 
-function omitOffensiveTerms(passage, offensiveTerms) {
-	const passageArray = passage.split(" ");
-	let newPassage = "The four men split up and walked off in different directions.";
-	passageArray.forEach(function(element) {
-		if (element.trim()) {
-			newPassage;
-		}
-	});
-	return newPassage;
-}
+// function omitOffensiveTerms(passage, offensiveTerms) {
+// 	const passageArray = passage.split(" ");
+// 	let newPassage = "The four men split up and walked off in different directions.";
+// 	passageArray.forEach(function(element) {
+// 		if (element.trim()) {
+// 			newPassage;
+// 		}
+// 	});
+// 	return newPassage;
+// }
 
-const passage = "zoinks muppeteer biffaroni loopdaloop The four men split up and walked off in different directions.";
-const offensiveTerms = "zoinks muppeteer biffaroni loopdaloop";
-console.log("New passage: ", omitOffensiveTerms(passage, offensiveTerms));
+// const passage = "zoinks muppeteer biffaroni loopdaloop The four men split up and walked off in different directions.";
+// const offensiveTerms = "zoinks muppeteer biffaroni loopdaloop";
+// console.log("New passage: ", omitOffensiveTerms(passage, offensiveTerms));
