dce6fcdd2dbc8faaa371790a15012a88ed1cb581
Brian Scherner <brianscherner@gmail.com>
2023-05-22 16:07:32 -0700

    Edit variable in wordCounter function and textArray variable. Declare mostUsedWords function with textArray, wordArray and countArray. Take textArray and make it uppercase, split each word and sort alphabetically. Write forEach loop method for each array to sort words by number of times each word is used.

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,12 @@ function isEmpty(testString) {
 	return (testString.trim().length === 0);
 }
 
-function wordCounter(inputText) {
+function wordCounter(text) {
 	if (isEmpty(text)) {
 		return 0;
 	}
 	let wordCount = 0;
-	const textArray = inputText.split(" ");
+	const textArray = text.split(" ");
 	textArray.forEach(function(element) {
 		if(!Number(element)) {
 			wordCount++;
@@ -51,26 +51,63 @@ function boldPassage(word, text) {
 		return p;
 	}
 
-// Practice function for omitting words from passage//
+	//step 1: write a function that returns how many times each word is used in a passage of text.
+	//step 2: organize the output of words from most used to least used.
 
-// function omitOffensiveTerms(passage, offensiveTerms) {
-// 	const passageArray = passage.split(" ");
-// 	let newPassage = "The four men split up and walked off in different directions.";
-// 	passageArray.forEach(function(element) {
-// 		if (element.trim()) {
-// 			newPassage;
-// 		}
-// 	});
-// 	return newPassage;
-// }
+function mostUsedWords(text) {
+	let textArray = text.toUpperCase().split(" ").sort();
+	let wordArray = [];
+	let countArray = [];
+	// split textArray into unique word array //
+	textArray.forEach(function(word) {
+		if (!wordArray.includes(word)) {
+			wordArray.push(word)
+		}
+	});
+	console.log("textArray: ", textArray);
+	console.log("wordArray: ", wordArray);
+	let counter = 0;
+	// loop through wordArray to compare each word to textArray //
+	wordArray.forEach(function(word) {
+		// if word = textArray(element), increase counter by 1 //
+		textArray.forEach(function(element) {
+			if (element === word) {
+				counter++;
+			}
+		});
+		countArray.push(counter);
+		counter = 0;
+	});
+	console.log("countArray: ", countArray);
+	let result = [];
+	wordArray.forEach(function(word, index) {
+		result.push(word + ": " + countArray[index])
+	});
+	console.log("Result before sort: ", result);
+	result.sort(function(word1, word2) {
+		let num1Array = word1.split(" ");
+		let num2Array = word2.split(" ");
+		let num1 = parseInt(num1Array[1]);
+		let num2 = parseInt(num2Array[1]);
+		console.log("word1 ", word1, num1);
+		console.log("word2", word2, num2);
+		if (num1 > num2) {
+			return -1;
+		}
+		if (num1 < num2) {
+			return 1;
+		}
+		return 0;
 
-// const passage = "zoinks muppeteer biffaroni loopdaloop The four men split up and walked off in different directions.";
-// const offensiveTerms = "zoinks muppeteer biffaroni loopdaloop";
-// console.log("New passage: ", omitOffensiveTerms(passage, offensiveTerms));
+	});
+	console.log("Sorted result: ", result);
+	return result;
+}
 
 function handleFormSubmission(event) {
 	event.preventDefault();
 	const passage = document.getElementById("text-passage").value;
+	const usedWords = mostUsedWords(passage);
 	const word = document.getElementById("word").value;
 	const wordCount = wordCounter(passage);
 	const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
